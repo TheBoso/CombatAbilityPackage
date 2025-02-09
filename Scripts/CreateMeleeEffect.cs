cff01000914e3acdd77d7c1657e4550f47c1a341
Dom <domenicomandica2000@gmail.com>
2025-02-09 19:11:49 +1030

    important fixes for melee attacks

--- Scripts/CreateMeleeEffect.cs
+++ Scripts/CreateMeleeEffect.cs
@@ -9,8 +9,6 @@ namespace YAAS
     public class CreateMeleeEffect : AbilityEffect
     {
         //  Where to create the overlap sphere
-        [SerializeField] private Vector3 _offset;
-        [SerializeField] private string OverlapObjectName;
         [SerializeField] private LayerMask OverlapMask;
         [SerializeField] private int BaseDamage = 1;
         [SerializeField] private ParticleSystem[] OnHitParticle;
@@ -19,10 +17,22 @@ namespace YAAS
 
         public override IEnumerator PerformEffect(AbilityCaster caller)
         {
-            Transform targetObject = caller.transform.Find(OverlapObjectName);
+            //  Something needs this component in the model
+            Transform targetObject = caller.GetComponentInChildren<CombatSpawnPoint>().transform;
             Vector3 overlapPos =
-                targetObject != null ? targetObject.position + _offset : caller.transform.position + _offset;
+                targetObject != null ? targetObject.position  : caller.transform.position;
 
+            #if UNITY_EDITOR
+            var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.localScale = Vector3.one * _radius;
+            sphere.transform.position = overlapPos;
+            Collider[] sphereCol = sphere.GetComponentsInChildren<Collider>();
+            foreach (var c in sphereCol)
+            {
+                Destroy(c);
+            }
+            Destroy(sphere, 3.0f);
+            #endif
             Collider[] colliders = Physics.OverlapSphere(overlapPos, _radius, OverlapMask);
 
             foreach (var hit in colliders)
@@ -44,8 +54,10 @@ namespace YAAS
 
                         Destroy(particle, 3.0f);
 
-                        health.Damage(BaseDamage, caller.transform, CriticalHit: Random.value <= 0.15f);
                     }
+                    
+                    health.Damage(BaseDamage, caller.transform, CriticalHit: Random.value <= 0.15f);
+
                 }
             }
 
