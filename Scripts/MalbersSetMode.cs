862e50dab7eeeb81f4cab1811bf4f75dc622e13b
Dom <domenicomandica2000@gmail.com>
2025-02-13 21:33:17 +1030

    Malbers set mode

---
+++ Scripts/MalbersSetMode.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using MalbersAnimations.Controller;
+using UnityEngine;
+
+namespace YAAS
+{
+    
+public class MalbersSetMode : AbilityEffect
+{
+    [SerializeField] private Mode _mode;
+    [SerializeField] private float _time = 0.0f;
+    [SerializeField] private AbilityStatus _status = AbilityStatus.PlayOneTime;
+    
+    public override IEnumerator PerformEffect(AbilityCaster caller)
+    {
+        if (caller.TryGetComponent(out MAnimal animal))
+        {
+            animal.Mode_TryActivate(_mode.ID, -99, _status, time: _time);
+        }
+
+        yield return null;
+    }
+}
+}
